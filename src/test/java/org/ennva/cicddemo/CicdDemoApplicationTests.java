c51832bdec596ab7fd28a4254233a1dd4368df09
Ennvaweb <35902574+ennva@users.noreply.github.com>
2022-07-06 16:12:27 +0200

    Initialize application

---
+++ src/test/java/org/ennva/cicddemo/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ennva.cicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

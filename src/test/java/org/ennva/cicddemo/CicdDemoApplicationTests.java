8d621a27a5120f6d0218214aeac56b3ab747715d
Ennvaweb <35902574+ennva@users.noreply.github.com>
2022-07-07 16:13:11 +0200

    Code Analysis with jacoco

--- src/test/java/org/ennva/cicddemo/CicdDemoApplicationTests.java
+++ src/test/java/org/ennva/cicddemo/CicdDemoApplicationTests.java
@@ -3,11 +3,19 @@ package org.ennva.cicddemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootTest
 class CicdDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void applicationStartTest() {
+		CicdDemoApplication.main(new String[] {"Hello", "World"});
 	}
 
 }
